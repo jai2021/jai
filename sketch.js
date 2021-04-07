1404de6a1f131875941cf492e14a0fc74f106031
jai2021 <79399165+jai2021@users.noreply.github.com>
2021-04-07 19:07:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,62 +1,66 @@
-var canvas;
-var block1,block2,block3,block4;
-var ball, edges;
-var music;
-
-function preload(){
-    // load sound here
+var cat,catImg;
+var mouse,mouseImg;
+var garden,gardenImg;
+var catrun,mouserun,mouse2,mouse3;
+var invisible;
+function preload() {
+  //load the images here
+  gardenImg=loadImage("garden.png");
+  catImg=loadImage("cat1.png");
+  mouseImg=loadImage("mouse4.png");
+  catrun=loadAnimation("cat2.png","cat3.png");
+  mouserun=loadAnimation("mouse4.png");
+  mouse2=loadAnimation("mouse1.png");
+  mouse3=loadAnimation("mouse2","mouse3");
 }
-
 
 function setup(){
-    canvas = createCanvas(800,600);
+    createCanvas(800,800);
+    //create tom and jerry sprites here
+    garden = createSprite(450,450);
+    garden.addImage(gardenImg);
+    garden.scale=1;
 
-    block1 = createSprite(0,580,360,30);
-    block1.shapeColor = "blue";
-
-    block2 = createSprite(295,580,200,30);
-    block2.shapeColor = "orange";
-
-    block3 = createSprite(510,580,200,30);
-    block3.shapeColor = "yellow";
-
-    block4 = createSprite(730,580,200,30);
-    block4.shapeColor = "red";
-
-    //create two more blocks i.e. block3 and block4 here
-
-    ball = createSprite(random(20,750),100, 40,40);
-    ball.shapeColor = rgb(255,255,255);
-    ball.velocityX=5;
-    ball.velocityY=5;
-    //write code to add velocityX and velocityY
-
-}
-
-function draw() {
-    background("black");
-    edges=createEdgeSprites();
-    ball.bounceOff(edges);
-
+    cat = createSprite(700,700);
+    cat.addImage(catImg);
+    cat.scale=0.1;
+    cat.setCollider("rectangle",0,0,cat.width,cat.height);
+    cat.debug = true;
     
-    //write code to bounce off ball from the block1 
-    if(block1.isTouching(ball) && ball.bounceOff(block1)){
-        ball.shapeColor = "blue";
-    }
-    if(block2.isTouching(ball) && ball.bounceOff(block2)){
-        ball.shapeColor = "orange";
-    }
-    if(block3.isTouching(ball) && ball.bounceOff(block3)){
-        ball.shapeColor = "yellow";
-    }
-    if(block4.isTouching(ball) && ball.bounceOff(block4)){
-        ball.shapeColor = "red";
-    }
-    //write code to bounce off ball from the block3
 
+    mouse = createSprite(100,700);
+    mouse.addImage(mouseImg);
+    //mouse.velocityX=3;
+    mouse.scale=0.1;
+    //mouse.setCollider("rectangle",0,0,mouse.width,mouse.height);
+    //mouse.debug = true;
+}
+function draw() {
+   background(255);
+    //Write condition here to evalute if tom and jerry collide
 
-    //write code to bounce off ball from the block4
-
+    if(cat.x-mouse.x<=(cat.width - mouse.width)/2){
+        cat.x = 700;
+        cat.velocityX=0;
+        mouse.x=100;   
+       }
+       keyPressed();
     drawSprites();
 }
 
+
+function keyPressed(){
+  //For moving and changing animation write code here
+  if(keyDown("LEFT_ARROW")){
+   cat.addAnimation("catrun",catrun);
+   cat.changeAnimation("catrun");
+   cat.velocityX=-3;
+
+}
+   if(keyDown("RIGHT_ARROW")){
+    mouse.addAnimation("mouse2",mouse2);
+    mouse.changeAnimation("mouse2");
+
+   }
+
+}
